b3dcb5851d2ea82d9c5bc6a55b6aed7f957ebaac
ArfinHasib <arfinhasib21375@gmail.com>
2026-02-08 22:52:21 +0600

    MongoDB Integration

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = 5000;
+
+app.get('/products', (req, res) => {
+	res.send('<h1>Hello World</h1>');
+});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log('Server started at Port,', PORT);
+});
