e09bd8b0b489cd6de812ecc4a284066184b0e851
ArfinHasib <arfinhasib21375@gmail.com>
2026-02-09 23:28:12 +0600

    Setting up /products endpoint

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,21 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
-app.get('/products', (req, res) => {
-	res.send('<h1>Hello World</h1>');
+app.post('/products', async (req, res) => {
+	const product = req.body; // User will send product data in the request body
+
+	if (!product.name || !product.price || !product.image) {
+		return res.status(400).json({ success: false, message: 'Name, price, and image are required fields.' });
+	}
+
+	const newProduct = new Product(product);
+
+	try {
+		await newProduct.save();
+		res.status(201).json({ success: true, data: newProduct });
+	} catch (error) {
+		console.error('Error saving product:', error);
+	}
 });
 
 console.log(process.env.MONGO_URI);
