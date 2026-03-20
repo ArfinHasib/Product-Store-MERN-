d31058f5a79d8cd8a22c63dda6d9a1a16dc8b49a
ArfinHasib <arfinhasib21375@gmail.com>
2026-03-20 12:02:24 +0600

    Navbar Init

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { Button, Box } from '@chakra-ui/react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import Navbar from './components/Navbar';
+
 function App() {
-	return <></>;
+	return (
+		<Box minH={'100vh'}>
+			{/* Navbar */}
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/create" element={<CreatePage />} />
+			</Routes>
+		</Box>
+	);
 }
 
 export default App;
