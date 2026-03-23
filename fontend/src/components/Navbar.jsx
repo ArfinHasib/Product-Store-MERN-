4f6335b24d5cad9890799f300d5da569460ed0d3
ArfinHasib <arfinhasib21375@gmail.com>
2026-03-23 22:38:35 +0600

    Navbar Init

--- fontend/src/components/Navbar.jsx
+++ fontend/src/components/Navbar.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { Button, Container, Flex, HStack, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { FiPlusSquare } from 'react-icons/fi';
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<Container maxWidth={'1140px'} px={'4'}>
+			<Flex
+				h={16}
+				alignItems={'center'}
+				justifyContent={'space-between'}
+				flexDir={{
+					base: 'column',
+					sm: 'row',
+				}}
+			>
+				<Text
+					fontSize={{ base: '22', sm: '28' }}
+					fontWeight={'bold'}
+					textTransform={'uppercase'}
+					textAlign={'center'}
+					bgGradient={'linear(to-r, cyan.400, blue.500)'}
+					bgClip={'text'}
+				>
+					<Link to="/">Product Store 🛒</Link>
+				</Text>
+
+				<HStack spacing={2} alignItems={'center'}>
+					<Link to="/create">
+						<Button>
+							<FiPlusSquare fontSize={20} />
+						</Button>
+						<Button>
+							<FiPlusSquare fontSize={20} />
+						</Button>
+					</Link>
+				</HStack>
+			</Flex>
+		</Container>
+	);
 };
 
 export default Navbar;
